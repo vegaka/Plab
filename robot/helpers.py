a5a9c4dc0ba0982c9339fcc1bc86b5d1a6592efe
JohannesAnd <JohannesAnd@users.noreply.github.com>
2015-10-21 12:12:02 +0200

    Remove logging/test

--- robot/helpers.py
+++ robot/helpers.py
@@ -1,4 +1,3 @@
-from Plab.imager.imager2 import Imager
 import statistics as s
 class Helpers:
 
@@ -30,7 +29,3 @@ class Helpers:
                 num += 1
             ret = ((total / num if num != 0 else 0)-image.xmax/2)/(image.xmax/2)
         return ret, numOfPixels/(image.xmax*image.ymax)*100
-
-h = Helpers()
-im = Imager("Red.jpg")
-print(h.get_red(im))
