e47e462b59876ba3d5f7a38bf000b4b4731b462d
karsy <vegard.karstang@gmail.com>
2015-10-21 08:15:05 +0200

    Committed edited image manipulation files to get ready for merge.

--- imager/artMaker.py
+++ imager/artMaker.py
@@ -1,6 +1,9 @@
 from imager.imager2 import Imager
 from imager.channelChanger import ChannelChanger
 from imager.trippy_images import TrippyImages
+from imager.enhance import *
+from imager.selectiveColor import *
+from imager.mosaic import *
 
 class ArtMaker(object):
 
@@ -11,3 +14,19 @@ class ArtMaker(object):
 
     def loadImages(self, files):
         return [Imager(path) for path in files]
+
+
+
+fib = Imager("images/fibonacci.jpeg")
+stairs = Imager("images/stairs.jpeg")
+donald = Imager("images/donaldduck.jpeg")
+
+fibstair = combine(fib, stairs)
+# fibstair = selective_color(fibstair, (251, 226, 22), 0.8)
+fibstair.display()
+
+donald = mosaic2(donald, 4)
+donald = TrippyImages(donald)
+donald.randomly_invert_pixels()
+donald = donald.get_imager()
+donald.display()
