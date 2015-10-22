d64e7ba8cd610b722cdfa20cc0f369c5efa834b8
karsy <vegard.karstang@gmail.com>
2015-10-22 11:10:45 +0200

    Fixed turning, hopefully.

--- robot/stay_inside.py
+++ robot/stay_inside.py
@@ -47,4 +47,4 @@ class StayInside(object):
                         l = 1
                 elif i > 2:
                         r = 1
-        return [-0.3, -0.5] if not l else [-0.5, -0.3]
+        return [-0.7, -1] if not l else [-1, -0.7]
