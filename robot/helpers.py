f14010201d28934ce094f36a4875d9a0c875aac0
Johannes Andersen <johanes1@online.no>
2015-10-27 11:35:38 +0100

    Uncomment shit

--- robot/helpers.py
+++ robot/helpers.py
@@ -2,17 +2,17 @@ class Helpers:
 
     @staticmethod
     def get_red(image):
-        #xmax = image.size[0]
-        #y = int(image.size[1]/2)
-        xmax = image.xmax
-        y = int(image.xmax/2)
+        xmax = image.size[0]
+        y = int(image.size[1]/2)
+        #xmax = image.xmax
+        #y = int(image.xmax/2)
         reds = []
         sum = 0
         num = 0
         thr = 4
         for x in range(0, xmax, 16):
-            #pix = image.getpixel((x, y))
-            pix = image.get_pixel(x, y)
+            pix = image.getpixel((x, y))
+            #pix = image.get_pixel(x, y)
             d1 = pix[0]/pix[1] if pix[1] != 0 else 255
             d2 = pix[0]/pix[2] if pix[2] != 0 else 255
             if (d1 > thr) and (d2 > thr):
