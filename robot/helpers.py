9a9f337f717e22cd186ad290d66092f32b11d7fc
Johannes Andersen <johanes1@online.no>
2015-10-27 11:52:19 +0100

    Logging

--- robot/helpers.py
+++ robot/helpers.py
@@ -6,23 +6,24 @@ class Helpers:
         y = int(image.size[1]/2)
         #xmax = image.xmax
         #y = int(image.xmax/2)
-        reds = []
         sum = 0
         num = 0
         thr = 4
+        reds = []
         for x in range(0, xmax, 16):
             pix = image.getpixel((x, y))
             #pix = image.get_pixel(x, y)
             d1 = pix[0]/pix[1] if pix[1] != 0 else 255
             d2 = pix[0]/pix[2] if pix[2] != 0 else 255
             if (d1 > thr) and (d2 > thr):
-                reds.append(True)
                 sum += x
                 num += 1
+                reds.append(True)
             else:
                 reds.append(False)
         if num != 0:
             pos = ((sum/num) - (xmax/2))/(xmax/2)
         else:
             pos = 0
+        print(reds, pos)
         return (num*16/xmax)*1000, pos
