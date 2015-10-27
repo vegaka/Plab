8cefc1e850ce3bef7434aa602abc1e1ab406bfdb
Johannes Andersen <johanes1@online.no>
2015-10-27 10:46:37 +0100

    Fixes colorwhatever

--- robot/helpers.py
+++ robot/helpers.py
@@ -1,34 +1,30 @@
+from Plab.imager.imager2 import Imager
 
 class Helpers:
 
     @staticmethod
     def get_red(image):
-        rth = 70
-        pth = 35
-        rows = []
-        sums = []
-        total = 0
-        num = 0
-        numOfPixels = 0
         xmax = image.size[0]
-        ymax = image.size[1]
-        for y in range(ymax):
-            reds = []
-            sum = 0
-            div = 0
-            for x in range(xmax):
-                pix = image.getpixel((x, y))
-                is_red = (pix[0] > rth)
-                not_red = (pix[1]+pix[2])/2 < pth
-                reds.append(is_red and not_red)
-                sum += x*int(is_red)
-                div += int(is_red)
-                numOfPixels += int(is_red)
-            rows.append(reds)
-            ans = sum/div if div != 0 else 0
-            sums.append(int(ans))
-            total += int(ans)
-            if int(ans) != 0:
+        y = int(image.size[1]/2)
+        #xmax = image.xmax
+        #y = int(image.xmax/2)
+        reds = []
+        sum = 0
+        num = 0
+        thr = 4
+        for x in range(xmax):
+            pix = image.getpixel((x, y))
+            #pix = image.get_pixel(x, y)
+            d1 = pix[0]/pix[1] if pix[1] != 0 else 255
+            d2 = pix[0]/pix[2] if pix[2] != 0 else 255
+            if (d1 > thr) and (d2 > thr):
+                reds.append(True)
+                sum += x
                 num += 1
-            ret = ((total / num if num != 0 else 0)-xmax/2)/(xmax/2)
-        return ret, numOfPixels/(xmax*ymax)*100
+            else:
+                reds.append(False)
+        if num != 0:
+            pos = ((sum/num) - (xmax/2))/(xmax/2)
+        else:
+            pos = 0
+        return pos, (num/xmax)*1000
